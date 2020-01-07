751ec2c465229b4a520778aeb9f7cb08b5c67ae2
Robert <bibbycodes@gmail.com>
2020-01-07 16:18:40 +0000

    Apple Price on Index.html

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 
 $(document).ready(() => {
-  
   $.get("http://localhost:5000/finance", (data) => {
     $(".price").append(data[0].bidPrice)
     console.log(data)
   })
-  
   console.log("connected")
 })
 
