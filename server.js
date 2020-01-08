f53014fd0c8c3789c54240934cb9d503804d6326
Robert <bibbycodes@gmail.com>
2020-01-08 15:11:22 +0000

    can fetch data from qoutes endpoint

--- server.js
+++ server.js
@@ -10,7 +10,38 @@ require('dotenv').config()
 
 app.use(bodyParser.json())
 
+async function getIEX(symbol) {
+  try {
+    let result = await axios.get(`https://cloud.iexapis.com/stable/tops?token=${process.env.API_KEY}&symbols=${symbol}`)
+    res.json(result.data)
+  } catch (err) {
+    console.log(err)
+  }
+}
 
+async function fetchQuote(symbol) {
+  let key = process.env.AV_KEY
+  let endpoint = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${key}`
+  let result = await axios.get(endpoint)
+  if (result.data["Global Quote"]) {
+    let data = {
+      symbol : result.data["Global Quote"]["01. symbol"],
+      open : result.data["Global Quote"]["02. open"],
+      high : result.data["Global Quote"]["03. high"],
+      low : result.data["Global Quote"]["04. low"],
+      price : result.data["Global Quote"]["05. price"],
+      volume : result.data["Global Quote"]["06. volume"],
+      last_trade_day : result.data["Global Quote"]['07. latest trading day'],
+      prev_close : result.data["Global Quote"]['08. previous close'],
+      change : result.data["Global Quote"]['09. change'],
+      percent_change : result.data["Global Quote"]['10. change percent']
+    }
+    return data
+  } else {
+    return result.data
+  }
+
+}
 
 // app.get('/', async (req, res) => {
 //   res.sendFile(path.join(__dirname, "client", "index.html"));
@@ -19,16 +50,8 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, "client")))
 
 app.get('/finance/:symbol', async (req, res) =>{
-  try {
-    res.header("Access-Control-Allow-Origin", "*");
-    console.log(req.params)
-    let symbol = req.params.symbol
-    let result = await axios.get(`https://cloud.iexapis.com/stable/tops?token=${process.env.API_KEY}&symbols=${symbol}`)
-    res.json(result.data)
-  } catch (err) {
-    console.log(err)
-  }
-
+  let symbol = req.params.symbol
+  res.json(await fetchQuote(symbol))
 })
 
 server.listen(port, () => console.log(`Listening on port: ${port}`))
