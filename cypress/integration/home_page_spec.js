a444d7ea04193195134a17a05e42066753236ed0
Robert <bibbycodes@gmail.com>
2020-01-08 18:02:06 +0000

    cypress default tests removed

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,19 +1,13 @@
 describe('Home page', () => {
   it('loads welcome text', () => {
     cy.visit('http://localhost:3000')
-
     cy.get('h1')
-
     cy.should('have.text', 'Cash Cow - Mooooo')
-
   })
 
   it('sees the price component', () => {
     cy.visit('http://localhost:3000')
-
     cy.get('.symbols')
-
     cy.should('contain', 'aapl')
-
   })
 })
