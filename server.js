f21537a5c6c3dff3abfea7015652e3ee87c7f517
Robert <bibbycodes@gmail.com>
2020-01-09 16:15:46 +0000

    can search for articles using company name

--- server.js
+++ server.js
@@ -16,20 +16,26 @@ app.use('/', express.static(path.join(__dirname, 'client')))
 
 app.get('/finance/:symbol', async (req, res) =>{
   let symbol = req.params.symbol
-  console.log(res.data)
   res.json(await DataFetcher.fetchQuote(symbol))
 })
 
 app.get('/news/:symbol', async (req, res) => {
   let symbol = req.params.symbol
-  // res.json(await NewsFetcher.fetchArticles)
-  let result = await NewsFetcher.fetchArticles(symbol)
-  console.log(result)
+  let details = await DataFetcher.fetchCompanyDetails(symbol)
+  let name = DataFetcher.getEncodedName(details)
+  let result = await NewsFetcher.fetchArticles(name)
+  res.status(200).send(result.articles)
 })
 
 app.get('/week/:symbol', async (req, res) => {
   let symbol = req.params.symbol
   res.json(await DataFetcher.fetchWeekData(symbol))
+})
+
+app.get('/finance/details/:symbol', async (req, res) => { //get company details
+  let symbol = req.params.symbol
+  let result = await DataFetcher.fetchCompanyDetails(symbol)
+  res.status(200).send(result)
 })
 
 server.listen(port, () => console.log(`Listening on port: ${port}`))
