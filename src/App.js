355020dd00b5c6564f1a37c5ccf35f26c2abe1bf
Emma Van Oss <e.vanoss@gmail.com>
2020-01-09 12:40:04 +0000

    renders a Graph components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Price from './Components/Price'
+import Graph from './Components/Graph'
 import './App.css';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      symbols: ["aapl", "googl"]
+      symbols: ["googl"]
     }
   }
 
@@ -14,11 +15,14 @@ class App extends React.Component {
     return (
       <div>
         <h1>Cash Cow - Mooooo</h1>
-
-        {this.state.symbols.map((symbol) => (
-          <Price symbol={symbol}/>
-        ))}
-
+        <div>
+          {this.state.symbols.map((symbol) => (
+            <div>
+              <Price symbol={symbol}/>
+              <Graph symbol={symbol}/>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
