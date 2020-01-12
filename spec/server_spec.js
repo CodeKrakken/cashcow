309cededf88920af72ef4399468ab6db74702364
Amy Balmforth <amy.balmforth@gmail.com>
2020-01-12 17:33:48 +0000

    tested server route api/finance/symbol

--- spec/server_spec.js
+++ spec/server_spec.js
@@ -3,42 +3,42 @@ const port = process.env.PORT || 5000
 const app = express()
 const server = require('http').createServer(app);
 const axios = require('axios')
+const sinon = require('sinon')
 const Server = require('../server.js')
+const DataFetcher = require('../models/DataFetcher')
 
-describe("GET /finance/:symbol", () => {
-  it("respond with 200 on success", async () => {
-    let result = await axios.get('http://localhost:5000/api/finance/MSFT')
-    expect(result.status).toEqual(200)
-  });
-  it("respond with error message on fail", async () => {
-    let result = await axios.get('http://localhost:5000/api/finance/nonexistant')
-    expect(result.data).toEqual({
-    'Error Message': 'Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for GLOBAL_QUOTE.'
-    });
+describe("GET /api/finance/:symbol", () => {
+  it("calls DataFetcher.fetchQuote with the symbol", async () => {
+    const fetchQuote = sinon.stub(DataFetcher, "fetchQuote")
+    await axios.get('http://localhost:5000/api/finance/GOOGL')
+    fetchQuote.restore()
+    sinon.assert.calledWith(fetchQuote, "GOOGL")
   })
-  it("returns requested symbol", async () => {
+
+  it("respond with 200 on success", async () => {
+    const fetchQuote = sinon.stub(DataFetcher, "fetchQuote")
     let result = await axios.get('http://localhost:5000/api/finance/GOOGL')
-    expect(result.data.symbol).toEqual("GOOGL");
-  })
-})
-
-describe("GET /news/:symbol", () => {
-  it("respond with 200 on success", async () => {
-    let result = await axios.get('http://localhost:5000/api/news/MSFT')
+    fetchQuote.restore()
     expect(result.status).toEqual(200)
   });
-})
 
-describe("GET /week/:symbol", () => {
-  it("respond with 200 on success", async () => {
-    let result = await axios.get('http://localhost:5000/api/week/MSFT')
-    expect(result.status).toEqual(200)
-  });
-})
-
-describe("GET /finance/details/:symbol", () => {
-  it("respond with 200 on success", async () => {
-    let result = await axios.get('http://localhost:5000/api/finance/details/MSFT')
-    expect(result.status).toEqual(200)
-  });
+// describe("GET /api/news/:symbol", () => {
+//   it("respond with 200 on success", async () => {
+//     let result = await axios.get('http://localhost:5000/api/news/MSFT')
+//     expect(result.status).toEqual(200)
+//   });
+// })
+//
+// describe("GET /api/week/:symbol", () => {
+//   it("respond with 200 on success", async () => {
+//     let result = await axios.get('http://localhost:5000/api/week/MSFT')
+//     expect(result.status).toEqual(200)
+//   });
+// })
+//
+// describe("GET /api/finance/details/:symbol", () => {
+//   it("respond with 200 on success", async () => {
+//     let result = await axios.get('http://localhost:5000/api/finance/details/MSFT')
+//     expect(result.status).toEqual(200)
+//   });
 })
