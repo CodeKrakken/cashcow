5582f77effeb9f079e26fc70a943f160327bd411
Robert <bibbycodes@gmail.com>
2020-01-13 13:57:00 +0000

    deleted Graph component, changed paths to new Graph component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Price from './Components/Price'
-import Graph from './Components/_Graph'
+import Graph from './Components/Graph'
 import NewsContainer from './Components/NewsContainer'
 import './App.css';
 
