c4c2026adea1a64fe29364653a935d50a84cf5c8
Emma Van Oss <e.vanoss@gmail.com>
2020-01-13 10:23:05 +0000

    all api routes in server tested - calling backend functions & status 200

--- spec/server_spec.js
+++ spec/server_spec.js
@@ -28,7 +28,6 @@ describe("server routes", () => {
 
   describe("GET /api/news/:symbol", () => {
     it("calls functions on DataFetcher to get company details, get name, articles and parse articles", async () => {
-
       // stub functions
       const fetchCompanyDetails = sinon.stub(DataFetcher, "fetchCompanyDetails")
       fetchCompanyDetails.returns({data: {}})
@@ -36,41 +35,64 @@ describe("server routes", () => {
       getEncodedName.returns("Google,%20Inc.")
       const fetchArticles = sinon.stub(NewsFetcher, "fetchArticles")
       fetchArticles.returns({articles: []})
-      const parseArticles = sinon.stub(NewsFetcher, "parseArticles")
-
-      // send request to internal API
+      const parseArticles = sinon.stub(NewsFetcher, "parseArticles") // send request to internal API
       await axios.get('http://localhost:5000/api/news/GOOGL')
-      // restore functions - cleanup
-      fetchCompanyDetails.restore()
+      fetchCompanyDetails.restore() // restore functions - cleanup
       getEncodedName.restore()
       fetchArticles.restore()
       parseArticles.restore()
-      // check what's happened
+      // assertions
       sinon.assert.calledWith(fetchCompanyDetails, "GOOGL")
       sinon.assert.calledWith(getEncodedName, {data: {}})
       sinon.assert.calledWith(fetchArticles, "Google,%20Inc.")
       sinon.assert.calledWith(parseArticles, [])
-
-
     })
-  //   it("respond with 200 on success", async () => {
-  //     let result = await axios.get('http://localhost:5000/api/news/MSFT')
-  //     expect(result.status).toEqual(200)
-  //   });
-  // })
-  //
-  // describe("GET /api/week/:symbol", () => {
-  //   it("respond with 200 on success", async () => {
-  //     let result = await axios.get('http://localhost:5000/api/week/MSFT')
-  //     expect(result.status).toEqual(200)
-  //   });
-  // })
-  //
-  // describe("GET /api/finance/details/:symbol", () => {
-  //   it("respond with 200 on success", async () => {
-  //     let result = await axios.get('http://localhost:5000/api/finance/details/MSFT')
-  //     expect(result.status).toEqual(200)
-  //   });
-  })
 
+    it("responds with 200 on success", async () => {
+      // stubs
+      const fetchCompanyDetails = sinon.stub(DataFetcher, "fetchCompanyDetails")
+      const getEncodedName = sinon.stub(DataFetcher, "getEncodedName")
+      const fetchArticles = sinon.stub(NewsFetcher, "fetchArticles")
+      fetchArticles.returns({articles: []})
+      const parseArticles = sinon.stub(NewsFetcher, "parseArticles")
+      const result = await axios.get('http://localhost:5000/api/news/GOOGL') // send request to internal API
+      fetchCompanyDetails.restore() // cleanup
+      getEncodedName.restore()
+      fetchArticles.restore()
+      parseArticles.restore()
+      expect(result.status).toEqual(200) // assertion
+    });
+  });
+
+  describe("GET /api/week/:symbol", () => {
+    it("calls Datafetcher.fetchWeekData with the expected symbol", async () => {
+      const fetchWeekData = sinon.stub(DataFetcher, "fetchWeekData")
+      await axios.get('http://localhost:5000/api/week/GOOGL')
+      fetchWeekData.restore()
+      sinon.assert.calledWith(fetchWeekData, "GOOGL")
+    })
+
+    it("respond with 200 on success", async () => {
+      const fetchWeekData = sinon.stub(DataFetcher, "fetchWeekData")
+      const result = await axios.get('http://localhost:5000/api/week/GOOGL')
+      fetchWeekData.restore()
+      expect(result.status).toEqual(200)
+    });
+  })
+  
+  describe("GET /api/finance/details/:symbol", () => {
+    it("calls DataFetcher.fetchCompanyDetails with the symbol given", async () => {
+      const fetchCompanyDetails = sinon.stub(DataFetcher, "fetchCompanyDetails")
+      await axios.get('http://localhost:5000/api/finance/details/GOOGL')
+      fetchCompanyDetails.restore()
+      sinon.assert.calledWith(fetchCompanyDetails, "GOOGL")
+    });
+
+    it("respond with 200 on success", async () => {
+      const fetchCompanyDetails = sinon.stub(DataFetcher, "fetchCompanyDetails")
+      const result = await axios.get('http://localhost:5000/api/finance/details/GOOGL')
+      fetchCompanyDetails.restore()
+      expect(result.status).toEqual(200)
+    });
+  });
 })
