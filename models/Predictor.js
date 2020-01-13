6c045b9cb65ce0ae8455c740b23aeb1bf7c383fd
Robert <bibbycodes@gmail.com>
2020-01-13 14:43:59 +0000

    can calculate moving average for a given data size

---
+++ models/Predictor.js
@@ -0,0 +1,17 @@
+const DataFetcher = require('./DataFetcher')
+
+class Predictor {
+  static movingAverage(data, size) {
+    let closePrices = data.map((item) => {
+      return parseFloat(item.close)
+    })
+    closePrices = closePrices.slice(0, size)
+    let sum = closePrices.reduce((acc, curr) => {
+      return acc += curr
+    })
+    let movingAverage = parseFloat((sum / size).toFixed(2))
+    return movingAverage
+  }
+}
+
+module.exports = Predictor
