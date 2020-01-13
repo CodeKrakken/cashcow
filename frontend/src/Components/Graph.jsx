2c90a4d2fd06e7339f51bc5d54449f425efc936a
Robert <bibbycodes@gmail.com>
2020-01-13 13:57:00 +0000

    deleted Graph component, changed paths to new Graph component

--- frontend/src/Components/Graph.jsx
+++ frontend/src/Components/Graph.jsx
@@ -2,95 +2,152 @@ import React, { Component } from "react";
 import Axios from "axios";
 const d3 = require("d3")
 
-class Graph extends React.Component{
-  constructor(props) {
+class Graph extends React.Component {
+  constructor(props){
     super(props)
     this.state = {
+      lines : ["high", "open", "close", "low"],
+      colors : ["steelblue", "mediumturquoise", "coral", "beige"]
     }
   }
 
   componentDidMount() {
-    Axios.get(`api/week/${this.props.symbol}`)
-    .then(res => {
-      let result = res.data
-      this.setState({sevenDayData : result})
-    })
-    .then(() => {
+    d3.json(`/api/week/${this.props.symbol}`).then(data => {
+      let timeseries = []
 
-      const width = 700,
-            height = 500;
-
-      const minMaxDates = (sevenDayData) => {
-        return [
-          Date.parse(sevenDayData.closePrices[6].date),
-          Date.parse(sevenDayData.closePrices[0].date)
-        ]
-      }
-      const minMaxPrice = (sevenDayData) => {
-        let prices = sevenDayData.closePrices.map((dayObject) => {
-          return dayObject.closePrice
+      for (let i = 0; i < data.length; i++) {
+        timeseries.push({
+          //graph input array accepts array of objects in the following format
+          date: new Date(data[i].date),
+          high: data[i].high,
+          low: data[i].low,
+          open: data[i].open,
+          close: data[i].close,
+          volume: data[i].volume
         })
-        return [Math.min(...prices), Math.max(...prices)]
       }
 
-      const chart = d3
-        .select(this.chartRef)
-        .attr("width", width + 100)
-        .attr("height", height + 100)
-        .append("g")
-        .attr("transform", "translate(100, 0)");
+      // graph is drawn with the following functions
+      this.setState({ data : timeseries });
+      this.initializeChart()
+      this.handleGraphScale()
+      this.generateAxes()
+      this.generateCloseLine()
+      // this.state.lines.forEach((line, i) => {
+      //   this.generateLine(line, this.state.colors[i])
+      //   })
+      }
+    );
+  }
 
-      const x = d3
-        .scaleTime()
-        .domain(minMaxDates(this.state.sevenDayData))
-        .range([0, width - 20]);
+  initializeChart = () => {
+    const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+    const width = 800 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
+    const svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('width', width + margin['left'] + margin['right'])
+      .attr('height', height + margin['top'] + margin['bottom'])
+      .append('g')
+      .attr('transform', `translate(${margin['left']},  ${margin['top']})`);
 
-      const y = d3
-        .scaleLinear()
-        .domain([0, minMaxPrice(this.state.sevenDayData)[1]]) //max value
-        .range([height, 20]);
-
-      const graph = chart
-        .selectAll(".graph")
-        .data(this.state.sevenDayData)
-        .enter()
-        .append("g")
-        .attr("class", "graph");
-
-      graph
-        .append("path")
-        .attr("class", "line")
-        .attr("d", parentData => {
-          return d3
-            .line()
-            .curve(d3.curveBasis) // make points round, not sharp
-            .x(d => x(d.date))
-            .y(d => y(d.value))(parentData.values);
-        });
-
-      chart
-        .append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", `translate(0,${y(0) - 20})`)
-        .call(d3.axisBottom(x));
-
-      chart
-        .append("g")
-        .attr("class", "axis axis--y")
-        .attr("transform", `translate(0,0)`)
-        .call(d3.axisLeft(y));
+    this.setState({
+      svg : svg,
+      margin : margin,
+      width : width,
+      height : height
     })
   }
 
-  render() {
+
+  handleGraphScale = () => { 
+    const xMin = d3.min(this.state.data, d => {
+      return d['date'];
+    });
+    const xMax = d3.max(this.state.data, d => {
+      return d['date'];
+    });
+    const yMin = d3.min(this.state.data, d => {
+      return d['close'];
+    });
+    const yMax = d3.max(this.state.data, d => {
+      return d['high'];
+    });
+
+    const xScale = d3
+      .scaleTime()
+      .domain([xMin, xMax]) // size of range of x values
+      .range([0, this.state.width]); // chart width
+    const yScale = d3
+      .scaleLinear()
+      .domain([yMin - 4, yMax]) // size of range of y values
+      .range([this.state.height, 0]); // chart height
+
+    this.setState({
+      yScale,
+      xScale
+    })
+  }
+
+  generateAxes = () => {
+    this.state.svg
+      .append('g')
+      .attr('id', 'xAxis')
+      .attr('transform', `translate(0, ${this.state.height})`)
+      .call(d3.axisBottom(this.state.xScale));
+
+    this.state.svg
+      .append('g')
+      .attr('id', 'yAxis')
+      .attr('transform', `translate(${this.state.width}, 0)`)
+      .call(d3.axisRight(this.state.yScale));
+  }
+
+  generateCloseLine = () => {
+    const line = d3.line()
+      .x(d => { // draw x line
+        return this.state.xScale(d['date']);
+      })
+      .y(d => { //draw y line
+        return this.state.yScale(d['close']);
+      });
+
+    this.state.svg
+      .append('path')
+      .data([this.state.data])
+      .style('fill', 'none')
+      .attr('id', 'priceChart')
+      .attr('stroke', 'steelblue')
+      .attr('stroke-width', '1.5')
+      .attr('d', line);
+  }
+
+  generateLine = (metric, color) => {
+    const line = d3.line()
+      .x(d => { // draw x line
+        return this.state.xScale(d['date']);
+      })
+      .y(d => { //draw y line
+        return this.state.yScale(d[metric]);
+      });
+
+    this.state.svg
+      .append('path')
+      .data([this.state.data])
+      .style('fill', 'none')
+      .attr('id', 'priceChart')
+      .attr('stroke', color)
+      .attr('stroke-width', '1.5')
+      .attr('d', line);
+  }
+  
+  render () {
     return(
-      <div>
-        <p>
-          {this.state.graph}
-        </p>
-        <svg
-          className='line-chart'
-          ref={r => (this.chartRef = r)} />
+      <div className="chart-container">
+        <div id="chart">
+          <h1>Chart</h1>
+        </div>
       </div>
     )
   }
