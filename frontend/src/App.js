f06d6c64059ad814bb1bc187aec67dfda0c0c3f8
Robert <bibbycodes@gmail.com>
2020-01-14 21:27:33 +0000

    can login logout WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,7 +32,6 @@ class App extends React.Component {
   }
 
   authenticate = (res) => {
-    console.log(res)
     sessionStorage.setItem("userId", res.user.id)
     sessionStorage.setItem("sessiondId", res.sessionId)
     sessionStorage.setItem("username", res.user.username)
@@ -44,6 +43,7 @@ class App extends React.Component {
     this.setState({didLogin : true})
   }
 
+  // render links based on login
   signupLink = () => {
     let isAuthenticated = sessionStorage.getItem("isAuthenticated")
     if(isAuthenticated != true) {
@@ -55,6 +55,7 @@ class App extends React.Component {
 
   loginLink = () => {
     let isAuthenticated = sessionStorage.getItem("isAuthenticated") // use JWT instead?
+    console.log(isAuthenticated)
     if(isAuthenticated != true) {
       return(
         <Link to="/login/">Login</Link>
@@ -63,12 +64,18 @@ class App extends React.Component {
   }
 
   logoutLink = () => {
-    let isAuthenticated = sessionStorage.getItem("isAuthenticated")
-    if(isAuthenticated) {
+    let isAuthenticated = sessionStorage.getItem("isAuthenticated") // doesnt work! NB Session storage clears on close tab
+    console.log(sessionStorage)
+    console.log(isAuthenticated)
+    if(isAuthenticated == "true") {
       return(
         <Link onClick={this.handleLogout}>Log Out</Link>
       )
     }
+  }
+
+  handleLogout = () => {
+    sessionStorage.clear()
   }
 
   appendFailMessage = (event) => {
@@ -105,6 +112,7 @@ class App extends React.Component {
         <Router>
           { this.signupLink() }
           { this.loginLink() }
+          { this.logoutLink() }
 
           <Link to="/app">Main</Link>
           <Route path="/register" component={() => 
