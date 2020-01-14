69038b4b46eb00b918db1eb111f32d303ef6071e
Robert <bibbycodes@gmail.com>
2020-01-14 12:35:58 +0000

    can create UsersTable for tests

--- spec/TestHelper.js
+++ spec/TestHelper.js
@@ -4,12 +4,23 @@ cwd = process.cwd()
 
 class TestHelper {
   constructor() {
-    this.createTableUsers = fs.readFileSync(`${cwd}/db/migrations/02_CREATE_TABLE_USERS.sql`).toString()
-    this.createTableStocks = fs.readFileSync(`${cwd}/db/migrations/03_CREATE_TABLE_STOCKS.sql`).toString()
-    this.createTableFriends = fs.readFileSync(`${cwd}/db/migrations/04_CREATE_TABLE_FRIENDS.sql`).toString()
-    this.insertIntoUsers = fs.readFileSync(`${cwd}/db/migrations/05_INSERT_INTO_USERS.sql`).toString()
-    this.insertIntoStocks = fs.readFileSync(`${cwd}/db/migrations/06_INSERT_INTO_STOCKS.sql`).toString()
-    console.log(this.insertIntoUsers)
+    this.createTableUsersQuery = fs.readFileSync(`${cwd}/db/migrations/02_CREATE_TABLE_USERS.sql`).toString()
+    this.createTableStocksQuery = fs.readFileSync(`${cwd}/db/migrations/03_CREATE_TABLE_STOCKS.sql`).toString()
+    this.createTableFriendsQuery = fs.readFileSync(`${cwd}/db/migrations/04_CREATE_TABLE_FRIENDS.sql`).toString()
+    this.insertIntoUsersQuery = fs.readFileSync(`${cwd}/db/migrations/05_INSERT_INTO_USERS.sql`).toString()
+    this.insertIntoStocksQuery = fs.readFileSync(`${cwd}/db/migrations/06_INSERT_INTO_STOCKS.sql`).toString()
+  }
+
+  async createTableUsers() {
+    try {
+      let db = new DbConnection()
+      await db.start()
+      let result = await db.query(this.createTableUsers)
+      await db.close()
+      return result
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
 
