57b608534796114ceaea5b00c5caa603baed8baa
Robert <bibbycodes@gmail.com>
2020-01-14 12:03:25 +0000

    added insert statements to migrations

--- db/migrations/02_CREATE_TABLE_USERS.sql
+++ db/migrations/02_CREATE_TABLE_USERS.sql
@@ -1,9 +1,8 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
+  username VARCHAR(80),
   first VARCHAR(80),
   last VARCHAR(80),
-  username VARCHAR(80),
   email VARCHAR(160),
   password VARCHAR(160),
-  created_at TIMESTAMP
   );
