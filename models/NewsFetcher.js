e5108fffb4c1b462aefb0c1ed4f79c113c80be68
Emma Van Oss <e.vanoss@gmail.com>
2020-01-15 19:44:50 +0000

    bug fix - API call works properly

--- models/NewsFetcher.js
+++ models/NewsFetcher.js
@@ -5,7 +5,7 @@ class NewsFetcher {
     try {
       const key = process.env.NEWS_KEY
       const language = 'en'
-      const startDate = new Date(Date.now() - 12096e5) // 14 days in milliseconds
+      const startDate = new Date(Date.now() - 12096e5).toISOString() // 14 days in milliseconds
       const endpoint = `https://newsapi.org/v2/everything?q=${encodedName}&apiKey=${key}&from=${startDate}&language=${language}`
       const response = await axios.get(endpoint)
       return response.data
